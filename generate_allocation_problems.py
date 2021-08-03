ac60b10610aa1254fea0e575ebb29822c8a2041c
Calvin Brown <calvinlongbrown@gmail.com>
2021-08-03 13:49:13 -0700

    added cal poly scenarios

--- generate_allocation_problems.py
+++ generate_allocation_problems.py
@@ -1,4 +1,5 @@
 import random
+from risk_scores import calculate_risk
 
 random.seed(1)
 
@@ -10,7 +11,18 @@ def generate_problem(vaccine_supply, days, num_centers, cc_range, num_individual
     f.write("\n{}\n".format(days))
     for i in range(num_individuals):
         f.write("{} {}\n".format(i, random.randrange(risk_range[0], risk_range[1])))
+        
+def generate_cal_poly_scenario(vaccine_supply, days, num_centers, cc_range):
+    f = open("input/cal_poly_{}v_{}d_{}c.txt".format(vaccine_supply, days, num_centers), "w")
+    f.write("{}\n".format(vaccine_supply))
+    for i in range(num_centers):
+        f.write("{} ".format(random.randrange(cc_range[0], cc_range[1])))
+    f.write("\n{}\n".format(days))
+    individuals = calculate_risk("input/cal_poly_individuals.txt")
+    for ind in individuals:
+        f.write("{} {}\n".format(ind.ind_id, ind.risk_score))
 
-generate_problem(800, 10, 4, (15,25), 1000, (1, 22))
+#generate_problem(800, 10, 4, (15,25), 1000, (1, 22))
+generate_cal_poly_scenario(25000, 40, 2, (250,251))
 
     
